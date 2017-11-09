57709b01c2e856ee29f7d9204be3c24ce9bf826f
Mike Persaud <michaelpersaud711@gmail.com>
2017-11-08 20:07:18 -0500

    corrected regex and crawling using BFS

--- src/com/webcrawler/Main.java
+++ src/com/webcrawler/Main.java
@@ -2,7 +2,8 @@ package com.webcrawler;
 
 public class Main {
     public static void main(String[] args) {
-            webCrawler crawler= new webCrawler();
-        System.out.println(( crawler.readUrl("https://facebook.com")));
+        webCrawler crawler= new webCrawler();
+        crawler.findConnectedSites("https://www.nbc.com");
+        System.out.println("Finished");
     }
 }
--- src/com/webcrawler/webCrawler.java
+++ src/com/webcrawler/webCrawler.java
@@ -1,29 +1,43 @@
 package com.webcrawler;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class webCrawler {
     Queue<String> queue;
     Set<String> set;
+
+    public webCrawler(){
+        queue=new LinkedList<>();
+        set = new HashSet<>();
+    }
     public String readUrl(String s){
         String htmlText="";
-        URL url = null;
+        URL url;
         try {
             url = new URL(s);
             BufferedReader br= new BufferedReader(new InputStreamReader(url.openStream()));
-            String a;
+
             while(br.readLine()!=null) {
 
                htmlText+=br.readLine();
+
             }
-        } catch (Exception e) {
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
 
         return htmlText;
 
@@ -32,9 +46,29 @@ public class webCrawler {
     public void findConnectedSites(String root){
 
         this.queue.add(root);
+        set.add(root);
 
         while(!queue.isEmpty()){
+            String removed= queue.remove();
 
+            String htmlText = readUrl(removed);
+
+            //grabbing urls in the form of http(s)://testwebsite.com
+            String regex= "http(s)?:\\/\\/www.\\w*.com";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher m = pattern.matcher(htmlText);
+
+            while(m.find()){
+
+                String found = m.group();
+                if(!set.contains(found)){
+                    set.add(found);
+                    System.out.println("New URL Found :"+found);
+                    queue.add(found);
+
+                }
+
+            }
         }
 
 
