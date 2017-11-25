1b4d1b99b9dd6229547942e0c056c579e6b2b23e
Mike Persaud <michaelpersaud711@gmail.com>
2017-11-25 03:04:15 -0500

    updation

---
+++ src/com/mst_prim/graphNode.java
@@ -0,0 +1,11 @@
+package com.mst_prim;
+
+public class graphNode {
+    int id;
+    graphNode next;
+
+    graphNode(int id){
+        this.id=id;
+
+    }
+}
---
+++ src/com/mst_prim/undirectedEdge.java
@@ -0,0 +1,4 @@
+package com.mst_prim;
+
+public class undirectedEdge {
+}
