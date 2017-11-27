f8721222e9467ac4f5781998f3374943ce597113
Nicole Latchman <nicoleslatchman@gmail.com>
2017-11-27 14:15:09 -0500

    Update Main.java
    Inserted args check, fixed indentation.

--- src/com/mst_prim/Main.java
+++ src/com/mst_prim/Main.java
@@ -4,28 +4,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
+        if (args.length < 1){
+            System.out.println("Error: Missing data file.");
+            System.exit(1);
+        }
+
         System.out.println("run forest run");
-        int numNodes= 0;
+
+        int numNodes;
         try {
             Scanner s = new Scanner(new File(args[0]));
             numNodes=Integer.parseInt(s.nextLine());
             primMST mst = new primMST(numNodes);
-            while(s.hasNext()){
 
+            while(s.hasNext()){
                 int i = Integer.valueOf(s.next());
                 int j = Integer.valueOf(s.next());
                 int cost = Integer.valueOf(s.next());
 
                 undirectedEdge eIn = new undirectedEdge(i,j,cost);
-                undirectedEdge eOut = new undirectedEdge(j,i,cost);
+//                undirectedEdge eOut = new undirectedEdge(j,i,cost);
                 mst.insertEdge(eIn);
                 mst.printList(mst.edgeListHead);
                 //mst.insertEdge(eOut);
                 //mst.printList(mst.edgeListHead);
-
             }
+
             System.out.println();
             mst.primAlgorithm();
 
@@ -37,10 +43,5 @@ public class Main{
         }
 
         System.out.println("finish him!");
-
     }
-
-
-
-
 }
